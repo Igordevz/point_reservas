c52445678518308556046f5f7d197aae3e09a521
Igordevz <programadorigor.dev@gmail.com>
2024-03-19 08:57:24 -0300

    additional new item model

--- server/src/models/point.ts
+++ server/src/models/point.ts
@@ -3,6 +3,8 @@ import { Document, Schema, model } from "mongoose";
 interface Ipoint extends Document {
   photo_point: [];
   admin_id: [];
+  address: string;
+  city: string;
   spam: number;
 }
 
@@ -10,6 +12,8 @@ export const CreateNewSchema = new Schema<Ipoint>({
   spam: { type: Number },
   photo_point: { type: [] },
   admin_id: { type: [] },
+  address: {type: String},
+  city: {type: String},
 });
 
 export const UserModel = model("points", CreateNewSchema);
