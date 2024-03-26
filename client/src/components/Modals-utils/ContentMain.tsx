672b83c3fe7cdee59252ce8f644d6dee3b600cd1
Igordevz <programadorigor.dev@gmail.com>
2024-03-26 09:08:57 -0300

    add notification

--- client/src/components/Modals-utils/ContentMain.tsx
+++ client/src/components/Modals-utils/ContentMain.tsx
@@ -3,7 +3,8 @@ import { AuthContextApi } from '@/context/Auth-context';
 import React, { useContext } from 'react'
 import { Avatar, AvatarFallback } from '../ui/avatar';
 import { Input } from '../ui/input';
-
+import { Button } from '../ui/button';
+import { BellIcon } from "@radix-ui/react-icons"
 export default function ContentMain() {
   const { user}: any = useContext(AuthContextApi);
 
@@ -12,13 +13,16 @@ export default function ContentMain() {
       <div className='w-[70%] border-r-[1px]'>
         <div className='p-4 flex flex-row justify-between border-b-[1px] '>
           <h2 className='font-extrabold'>Home</h2>
+          <Button variant="ghost">
+            <BellIcon/>
+          </Button>
         </div>
         <div className='flex flex-col'>
           <div className='p-4 flex flex-row gap-2 '>
             <Avatar>
               <AvatarFallback>{user?.name[0]}</AvatarFallback>
             </Avatar>
-            <Input className='border-none focus:ring-0 focus:border-transparent' placeholder='Deseja postar algo?' />
+            <Input className='border-none focus:ring-0 focus:border-transparent' placeholder='Procurar por amigos...' />
           </div>
         </div>
       </div>
